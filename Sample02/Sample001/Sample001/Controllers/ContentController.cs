e3b354f75428f4ba93adb4c632db5bd67cf30392
sparrowbill <xrchen_2008@163.com>
2018-12-14 17:20:06 +0800

    学习第三章的事例代码

---
+++ Sample02/Sample001/Sample001/Controllers/ContentController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Sample001.Models;
+
+namespace Sample001.Controllers
+{
+    /// <summary>
+    /// 2018.12.13
+    /// chenxr
+    /// Content控制器
+    /// </summary>
+    public class ContentController : Controller
+    {
+        private readonly Content contents;
+
+        public ContentController(IOptions<Content> options)
+        {
+            contents = options.Value;
+        }
+
+        /// <summary>
+        /// 首页显示
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            //var contents = new List<Content>();
+            //for (int i = 1; i < 11; i++)
+            //{
+            //    contents.Add(new Content { Id = i, title = $"{i}的标题", content = $"{i}的内容", status = 1, add_time = DateTime.Now.AddDays(-i)});
+            //}
+            return View(new ContentViewModel { Contents = new List<Content> { contents } });
+        }
+    }
+}
---
+++ Sample02/Sample001/Sample001/Models/ContentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Sample001.Models
+{
+    /// <summary>
+    /// 2018.12.13
+    /// chenxr
+    /// Content视图模式
+    /// </summary>
+    public class ContentViewModel
+    {
+        /// <summary>
+        /// 内容列表
+        /// </summary>
+        public List<Content> Contents { get; set; }
+    }
+}
